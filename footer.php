1d1e7b532dd40676de60ae20f45c0bac239af8f4
geetha arunkumar <geesslight18@gmail.com>
2018-10-07 20:12:09 -0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,8 @@
+<div class="container">
+ <div class="alert alert-info">
+    <strong>tutorial !</strong> <a href="http://www.codingcage.com/">Coding Cage</a>!
+ </div>
+</div>
+<script src="bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>PDO OOP CRUD using Bootstrap</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="navbar navbar-default navbar-static-top" role="navigation">
+    <div class="container">
+ 
+        <div class="navbar-header">
+            <a class="navbar-brand" href="http://www.codingcage.com" title='Programming Blog'>Coding Cage</a>
+            <a class="navbar-brand" href="http://www.codingcage.com/search/label/CRUD">CRUD</a>
+            <a class="navbar-brand" href="http://www.codingcage.com/search/label/PDO">PDO</a>
+            <a class="navbar-brand" href="http://www.codingcage.com/search/label/jQuery">jQuery</a>
+        </div>
+ 
+    </div>
+</div>
